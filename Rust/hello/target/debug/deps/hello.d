f99e03a1c1d405ac4d0296077b3994f7a143e197
unknown <kabilan@shamlatech.com>
2024-05-02 08:02:52 +0530

    update rust hello world

---
+++ Rust/hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\KABILAN\Documents\Solana\Rust\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\KABILAN\Documents\Solana\Rust\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\KABILAN\Documents\Solana\Rust\hello\target\debug\hello.exe: C:\Users\KABILAN\Documents\Solana\Rust\hello\src\main.rs
