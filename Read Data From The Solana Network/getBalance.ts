b23f63ec91fceb004435f38073fc329ba1dd0300
unknown <kabilan@shamlatech.com>
2024-05-02 08:06:27 +0530

    update solana cli

--- Read Data From The Solana Network/getBalance.ts
+++ Read Data From The Solana Network/getBalance.ts
@@ -1,7 +1,7 @@
 import { Connection, PublicKey, clusterApiUrl , LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 const connection = new Connection(clusterApiUrl("devnet"));
-const address = new PublicKey('CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN');
+const address = new PublicKey('J2aZ43z8dDBMZaMXyWiZUCLKoPgeNJHUyQSEJEML4WZe');
 const balance = await connection.getBalance(address);
 const balanceInSol = balance / LAMPORTS_PER_SOL;
 
